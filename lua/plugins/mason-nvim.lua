24552e9da6b07d23ade193efe92e87c31d4c8c54
Nico Pareigis <nico@pareigis.de>
2023-11-22 13:08:46 +0100

    fix(lsp): load mason on BufNewFile

--- lua/plugins/mason-nvim.lua
+++ lua/plugins/mason-nvim.lua
@@ -2,7 +2,7 @@ return {
   'williamboman/mason.nvim',
   lazy = true,
   cmd = 'Mason',
-  event = 'BufReadPost',
+  event = { 'BufReadPost', 'BufNewFile' },
   dependencies = { 'hrsh7th/cmp-nvim-lsp', 'neovim/nvim-lspconfig' },
   init = function()
     if vim.fn.argc() ~= 0 then require('mason') end
