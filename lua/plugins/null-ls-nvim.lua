803059e38c0144d707fe652ba79f9f47f5f9903b
Nico Pareigis <nico@pareigis.de>
2023-12-22 11:36:57 +0100

    feat: add stylua config

--- lua/plugins/null-ls-nvim.lua
+++ lua/plugins/null-ls-nvim.lua
@@ -4,58 +4,62 @@ return {
   event = { 'BufNewFile', 'BufReadPost' },
   dependencies = 'nvim-lua/plenary.nvim',
   config = function()
-    local mpc = vim.fn.stdpath('data')..'/mason/bin'
     local null = require('null-ls')
-    local cda = null.builtins.code_actions
-    local dgn = null.builtins.diagnostics
-    local fmt = null.builtins.formatting
+    local mpc = vim.fn.stdpath('data') .. '/mason/bin'
 
-      local eslint_d_conf = {
-        command = mpc..'/eslint_d',
+    local sources = {
+      eslint_d = {
+        command = mpc .. '/eslint_d',
         condition = function(utils)
           return utils.root_has_file({
             '.eslintrc.js',
             '.eslintrc.json',
-            '.eslintrc.yml'
+            '.eslintrc.yml',
           })
-        end
-      }
-
-      local prettierd_conf = {
-        command = mpc..'/prettierd',
+        end,
+      },
+      prettier_d = {
+        command = mpc .. '/prettierd',
         condition = function(utils)
           return utils.root_has_file({
             '.prettierrc',
             '.prettierrc.js',
             '.prettierrc.json',
-            '.prettierrc.yml'
+            '.prettierrc.yml',
           })
-        end
-      }
+        end,
+      },
+      stylua = {
+        command = mpc .. '/stylua',
+      },
+    }
 
     null.setup({
       on_attach = function(client, bufnr)
         if client.supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                group = vim.api.nvim_create_augroup('null-ls', { clear = true }),
-                buffer = bufnr,
-                desc = 'null-ls formatting',
-                callback = function()
-                  vim.lsp.buf.format({
-                    bufnr = bufnr,
-                    ---@diagnostic disable-next-line: redefined-local
-                    filter = function(client) return client.name == "null-ls" end
-                  })
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = vim.api.nvim_create_augroup('null-ls', { clear = true }),
+            buffer = bufnr,
+            desc = 'null-ls formatting',
+            callback = function()
+              vim.lsp.buf.format({
+                bufnr = bufnr,
+                ---@diagnostic disable-next-line: redefined-local
+                filter = function(client)
+                  return client.name == 'null-ls'
                 end,
-            })
+              })
+            end,
+          })
         end
       end,
 
       sources = {
-        cda.eslint_d.with(eslint_d_conf),
-        dgn.eslint_d.with(eslint_d_conf),
-        fmt.prettierd.with(prettierd_conf)
-      }
+        null.builtins.code_actions.eslint_d.with(sources.eslint_d),
+        null.builtins.diagnostics.eslint_d.with(sources.eslint_d),
+        null.builtins.formatting.prettierd.with(sources.prettier_d),
+        null.builtins.formatting.stylua.with(sources.stylua),
+      },
     })
-  end
+  end,
 }
