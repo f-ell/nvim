e5ca3f3b3c2a5b53ce872c4df5ef5b29e492e018
Nico Pareigis <nico@pareigis.de>
2023-12-22 14:50:44 +0100

    chore(telescope): lazy load on cmd

--- lua/plugins/telescope-nvim.lua
+++ lua/plugins/telescope-nvim.lua
@@ -1,8 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim',
   lazy = true,
+  cmd = 'Telescope',
   keys = {
-    { '<leader>b',  '<CMD>Telescope buffers<CR>' },
+    { '<leader>b', '<CMD>Telescope buffers<CR>' },
     { '<leader>ff', '<CMD>Telescope find_files<CR>' },
     { '<leader>fg', '<CMD>silent! Telescope git_files<CR>' },
     { '<leader>rg', '<CMD>Telescope live_grep<CR>' },
@@ -10,43 +11,51 @@ return {
       '<leader>ro',
       function()
         require('telescope.builtin').live_grep({ grep_open_files = true })
-      end
+      end,
     },
-    { '<leader>zf', '<CMD>Telescope current_buffer_fuzzy_find<CR>' }
+    { '<leader>zf', '<CMD>Telescope current_buffer_fuzzy_find<CR>' },
   },
   dependencies = {
     'natecraddock/telescope-zf-native.nvim',
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    local tls   = require('telescope')
-    local tslu  = require('telescope.utils')
+    local tls = require('telescope')
+    local tslu = require('telescope.utils')
     local tlsal = require('telescope.actions.layout')
 
     tls.setup({
       defaults = {
         initial_mode = 'normal',
         path_display = {
-          shorten = { len = 1, exclude = {-2, -1, 1} },
+          shorten = { len = 1, exclude = { -2, -1, 1 } },
           truncate = 1,
           vimgrep_arguments = {
-            'rg', '--color=never', '--no-heading', '-H',
-            '--column', '-n', '-S', '--hidden', '--max-depth', '8'
-          }
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '-H',
+            '--column',
+            '-n',
+            '-S',
+            '--hidden',
+            '--max-depth',
+            '8',
+          },
         },
 
-        sorting_strategy  = 'descending',
-        scroll_strategy   = 'limit',
+        sorting_strategy = 'descending',
+        scroll_strategy = 'limit',
         file_ignore_patterns = { '.cache/', 'undo/' },
 
-        borderchars       = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
-        layout_strategy   = 'vertical',
+        borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+        layout_strategy = 'vertical',
         theme = 'dropdown',
-        layout_config     = {
+        layout_config = {
           prompt_position = 'bottom',
-          width   = 0.80,
-          height  = 0.70,
+          width = 0.80,
+          height = 0.70,
           horizontal = {
             preview_width = 0.65,
             results_width = 0.3,
@@ -55,23 +64,23 @@ return {
             preview_height = 0.45,
             mirror = false,
           },
-          preview_cutoff = 12
+          preview_cutoff = 12,
         },
 
-        prompt_prefix     = ' ',
-        entry_prefix      = '  ',
-        selection_caret   = '> ',
-        winblend          = 0,
+        prompt_prefix = ' ',
+        entry_prefix = '  ',
+        selection_caret = '> ',
+        winblend = 0,
         preview = {
-          filesize_limit  = 0.3,
-          timeout         = 100,
-          hide_on_startup = true
+          filesize_limit = 0.3,
+          timeout = 100,
+          hide_on_startup = true,
         },
-        color_devicons    = true,
+        color_devicons = true,
 
         mappings = {
           i = {
-            ['<CR>']  = 'select_default',
+            ['<CR>'] = 'select_default',
             ['<C-s>'] = 'select_horizontal',
             ['<C-v>'] = 'select_vertical',
             ['<C-t>'] = 'select_tab',
@@ -82,79 +91,79 @@ return {
             ['<C-b>'] = 'preview_scrolling_up',
             ['<C-f>'] = 'preview_scrolling_down',
             ['<C-u>'] = false,
-            ['<C-d>'] = false
+            ['<C-d>'] = false,
           },
           n = {
             ['<ESC>'] = false,
-            ['<CR>']  = 'select_default',
+            ['<CR>'] = 'select_default',
             ['<C-s>'] = 'select_horizontal',
             ['<C-v>'] = 'select_vertical',
             ['<C-t>'] = 'select_tab',
             ['<C-c>'] = 'close',
-            ['j']     = 'move_selection_next',
-            ['k']     = 'move_selection_previous',
-            ['gg']    = 'move_to_top',
-            ['G']     = 'move_to_bottom',
+            ['j'] = 'move_selection_next',
+            ['k'] = 'move_selection_previous',
+            ['gg'] = 'move_to_top',
+            ['G'] = 'move_to_bottom',
             ['<C-p>'] = tlsal.toggle_preview,
             ['<C-b>'] = 'preview_scrolling_up',
             ['<C-f>'] = 'preview_scrolling_down',
             ['<C-u>'] = false,
-            ['<C-d>'] = false
-          }
-        }
+            ['<C-d>'] = false,
+          },
+        },
       },
       pickers = {
-        cwd     = tslu.buffer_dir(),
-        hidden  = true,
+        cwd = tslu.buffer_dir(),
+        hidden = true,
 
         buffers = {
-          initial_mode  = 'insert',
+          initial_mode = 'insert',
           results_title = false,
-          ignore_current_buffer = true
+          ignore_current_buffer = true,
         },
 
         find_files = {
-          initial_mode  = 'insert',
+          initial_mode = 'insert',
           results_title = false,
-          find_command  = { 'fd', '-tf', '-H', '-d10', '--strip-cwd-prefix' },
+          find_command = { 'fd', '-tf', '-H', '-d10', '--strip-cwd-prefix' },
         },
         git_files = {
           initial_mode = 'insert',
-          results_title = false
+          results_title = false,
         },
 
         live_grep = {
           initial_mode = 'insert',
           preview = {
-            filesize_limit  = 0.3,
-            timeout         = 100,
-            hide_on_startup = false
+            filesize_limit = 0.3,
+            timeout = 100,
+            hide_on_startup = false,
           },
         },
 
         current_buffer_fuzzy_find = {
-          initial_mode  = 'insert',
+          initial_mode = 'insert',
           results_title = false,
-          skip_empty_lines = true
-        }
+          skip_empty_lines = true,
+        },
       },
 
-      extensions  = {
+      extensions = {
         ['zf-native'] = {
           file = {
-            enable            = true,
+            enable = true,
             highlight_results = true,
-            match_filename    = true
+            match_filename = true,
           },
           generic = {
-            enable            = true,
+            enable = true,
             highlight_results = true,
-            match_filename    = true
-          }
-        }
-      }
+            match_filename = true,
+          },
+        },
+      },
     })
 
     tls.load_extension('zf-native')
-  end
+  end,
 }
