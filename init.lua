fdd02d8a75278c36d10385b50e208d28c48027d0
Nico Pareigis <nico@pareigis.de>
2023-12-22 11:47:48 +0100

    refactor: formatting

--- init.lua
+++ init.lua
@@ -2,13 +2,16 @@ local L = require('utils.lib')
 
 -- bootstrap
 local std = vim.fn.stdpath
-local path = std('data')..'/lazy/lazy.nvim'
+local path = std('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(path) then
   vim.fn.system({
-    'git', 'clone',
-    '--filter=blob:none', '--single-branch',
-    'https://github.com/folke/lazy.nvim.git', path
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--single-branch',
+    'https://github.com/folke/lazy.nvim.git',
+    path,
   })
 end
 
@@ -24,9 +27,9 @@ require('lazy').setup('plugins', {
   performance = {
     cache = {
       enabled = true,
-      path = std('cache')..'/lazy/cache'
-    }
-  }
+      path = std('cache') .. '/lazy/cache',
+    },
+  },
 })
 
 L.key.nnmap('<leader>*', '<CMD>Lazy<CR>')
