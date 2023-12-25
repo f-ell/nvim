a129a62c6a279aa0a7e25c52f780a9bab775fe02
Nico Pareigis <nico@pareigis.de>
2023-12-25 11:34:46 +0100

    chore: refactor highlights

--- lua/utils/highlights.lua
+++ lua/utils/highlights.lua
@@ -1,134 +1,143 @@
----@diagnostic disable: unused-local
-local set_hl_groups = function()
-  local _ = ''
+local c = {
+  _ = '',
 
-  local fg1 = '#fdf6e3'
-  local fg2 = '#d3c6aa'
-  local fg3 = '#859289'
-  local fg4 = '#434f55'
-  local fg5 = '#374247'
-  local fg6 = '#2a3439'
+  fg = {
+    '#fdf6e3',
+    '#d3c6aa',
+    '#859289',
+  },
+  bg = {
+    '#434f55',
+    '#374247',
+    '#2a3439',
+  },
 
-  local aqu1 = '#83c092'
-  local aqu2 = '#35a77c'
-  local blu1 = '#7fbbb3'
-  local blu2 = '#3a94c5'
-  local gre1 = '#a7c080'
-  local gre2 = '#8da101'
-  local pur1 = '#d699b6'
-  local pur2 = '#df69ba'
-  local red1 = '#e67e80'
-  local red2 = '#f85552'
-  local yel1 = '#dbbc7f'
-  local yel2 = '#dfa000'
+  pastel = {
+    aqu = '#83c092',
+    blu = '#7fbbb3',
+    gre = '#a7c080',
+    pur = '#d699b6',
+    red = '#e67e80',
+    yel = '#dbbc7f',
+  },
+  vivid = {
+    aqu = '#35a77c',
+    blu = '#3a94c5',
+    gre = '#8da101',
+    pur = '#df69ba',
+    red = '#f85552',
+    yel = '#dfa000',
+  },
+  misc = {
+    git = '#fca326',
+  },
+}
 
-  local git = '#fca326'
+local highlights = {
+  -- search
+  { 0, 'Search', { fg = c.bg[3], bg = c.pastel.gre } },
+  { 0, 'IncSearch', { fg = c.bg[3], bg = c.pastel.red } },
+  { 0, 'CurSearch', { link = 'IncSearch' } },
+  { 0, 'Substitute', { link = 'IncSearch' } },
+  { 0, 'Visual', { fg = c._, bg = '#3d5665' } },
 
-  local highlights = {
-    -- Search
-    { 0, 'Search', { fg = fg6, bg = gre1 } },
-    { 0, 'IncSearch', { fg = fg6, bg = red1 } },
-    { 0, 'CurSearch', { link = 'IncSearch' } },
-    { 0, 'Substitute', { link = 'IncSearch' } },
-    { 0, 'Visual', { fg = _, bg = '#3d5665' } },
+  -- diagnostics
+  { 0, 'ErrorText', { sp = c.pastel.red, underline = true } },
+  { 0, 'WarningText', { sp = c.pastel.yel, underline = true } },
+  { 0, 'InfoText', { sp = c.pastel.gre, underline = true } },
+  { 0, 'HintText', { sp = c.pastel.blu, underline = true } },
+  { 0, 'DiagnosticSignError', { fg = c.pastel.red } },
+  { 0, 'DiagnosticSignWarn', { fg = c.pastel.yel } },
+  { 0, 'DiagnosticSignInfo', { fg = c.pastel.gre } },
+  { 0, 'DiagnosticSignHint', { fg = c.pastel.blu } },
 
-    -- Diagnostics
-    { 0, 'ErrorText', { sp = red1, underline = true } },
-    { 0, 'WarningText', { sp = yel1, underline = true } },
-    { 0, 'InfoText', { sp = gre1, underline = true } },
-    { 0, 'HintText', { sp = blu1, underline = true } },
-    { 0, 'DiagnosticSignError', { fg = red1 } },
-    { 0, 'DiagnosticSignWarn', { fg = yel1 } },
-    { 0, 'DiagnosticSignInfo', { fg = gre1 } },
-    { 0, 'DiagnosticSignHint', { fg = blu1 } },
+  -- float
+  { 0, 'NormalFloat', { fg = c._, bg = c._ } },
+  { 0, 'FloatTitle', { fg = c.fg[2], bg = c._ } },
+  { 0, 'FloatBorder', { fg = c.fg[3], bg = c._ } },
 
-    -- Float
-    { 0, 'NormalFloat', { fg = _, bg = _ } },
-    { 0, 'FloatTitle', { fg = fg2, bg = _ } },
-    { 0, 'FloatBorder', { fg = fg3, bg = _ } },
+  { 0, 'NeutralFloat', { fg = c.fg[3], bg = c._ } },
 
-    { 0, 'NeutralFloat', { fg = fg3, bg = _ } },
+  -- statusline
+  { 0, 'Statusline', { fg = c.fg[2], bg = c._ } },
+  {
+    0,
+    'StatuslineReadonly',
+    { fg = c.pastel.red, bg = c._, bold = true },
+  },
+  { 0, 'StatuslineLspinfo', { fg = c.fg[3], bg = c._ } },
+  { 0, 'StatuslineBytecount', { fg = c.pastel.yel, bg = c._ } },
+  { 0, 'StatuslineSearch', { fg = c.pastel.blu, bg = c._ } },
+  { 0, 'StatuslineLocation', { fg = c.pastel.aqu, bg = c._ } },
 
-    -- Statusline
-    { 0, 'Statusline', { fg = fg2, bg = _ } },
-    { 0, 'StatuslineReadonly', { fg = red1, bg = _, bold = true } },
-    { 0, 'StatuslineLspinfo', { fg = fg3, bg = _ } },
-    { 0, 'StatuslineBytecount', { fg = yel1, bg = _ } },
-    { 0, 'StatuslineSearch', { fg = blu1, bg = _ } },
-    { 0, 'StatuslineLocation', { fg = aqu1, bg = _ } },
+  { 0, 'modeC', { fg = c.pastel.pur, bg = c._ } },
+  { 0, 'modeI', { fg = c.pastel.blu, bg = c._ } },
+  { 0, 'modeN', { fg = c.pastel.gre, bg = c._ } },
+  { 0, 'modeR', { fg = c.pastel.yel, bg = c._ } },
+  { 0, 'modeT', { fg = c.fg[3], bg = c._ } },
+  { 0, 'modeV', { fg = c.pastel.red, bg = c._ } },
 
-    -- Statusline Modes
-    { 0, 'modeC', { fg = pur1, bg = _ } },
-    { 0, 'modeI', { fg = blu1, bg = _ } },
-    { 0, 'modeN', { fg = gre1, bg = _ } },
-    { 0, 'modeR', { fg = yel1, bg = _ } },
-    { 0, 'modeT', { fg = fg3, bg = _ } },
-    { 0, 'modeV', { fg = red1, bg = _ } },
+  -- tabline
+  { 0, 'TlActive', { fg = c.fg[2], bg = c.bg[2], bold = true } },
+  { 0, 'TlInactive', { fg = c.fg[3], bg = c.bg[3] } },
 
-    -- Tabline
-    { 0, 'TlActive', { fg = fg2, bg = fg5, bold = true } },
-    { 0, 'TlInactive', { fg = fg3, bg = fg6 } },
+  -- misc
+  { 0, 'SpellBad', { sp = c.pastel.red, undercurl = true } },
+  { 0, 'Git', { fg = c.misc.git, bg = c._ } },
+  { 0, 'GitZero', { fg = c.fg[3], bg = c._ } },
+  { 0, 'GitAdd', { fg = c.pastel.gre, bg = c._ } },
+  { 0, 'GitCha', { fg = c.pastel.blu, bg = c._ } },
+  { 0, 'GitDel', { fg = c.pastel.red, bg = c._ } },
 
-    -- Misc
-    { 0, 'SpellBad', { sp = red1, undercurl = true } },
-    { 0, 'Git', { fg = git, bg = _ } },
-    { 0, 'GitZero', { fg = fg3, bg = _ } },
-    { 0, 'GitAdd', { fg = gre1, bg = _ } },
-    { 0, 'GitCha', { fg = blu1, bg = _ } },
-    { 0, 'GitDel', { fg = red1, bg = _ } },
+  ---- plugins ----
+  -- cmp
+  { 0, 'CmpItemMenu', { fg = c.fg[3], bg = c._ } },
+  { 0, 'CmpItemAbbrMatch', { fg = c.pastel.red, bg = c._ } },
+  { 0, 'CmpItemAbbrMatchFuzzy', { fg = c.pastel.red, bg = c._ } },
 
-    -- PLUGINS --
-    -- Cmp
-    { 0, 'CmpItemMenu', { fg = fg3, bg = _ } },
-    { 0, 'CmpItemAbbrMatch', { fg = red1, bg = _ } },
-    { 0, 'CmpItemAbbrMatchFuzzy', { fg = red1, bg = _ } },
+  -- dap
+  { 0, 'DapStopped', { fg = c.pastel.gre } },
+  { 0, 'DapLogPoint', { fg = c.pastel.yel } },
+  { 0, 'DapBreakpoint', { fg = c.pastel.red } },
+  { 0, 'DapBreakpointCondition', { fg = c.pastel.pur } },
+  { 0, 'DapBreakpointRejected', { fg = c.pastel.red } },
 
-    -- Dap
-    { 0, 'DapStopped', { fg = gre1 } },
-    { 0, 'DapLogPoint', { fg = yel1 } },
-    { 0, 'DapBreakpoint', { fg = red1 } },
-    { 0, 'DapBreakpointCondition', { fg = pur2 } },
-    { 0, 'DapBreakpointRejected', { fg = red2 } },
+  -- gitsigns
+  {
+    0,
+    'DiffText',
+    { bg = '#3d5665', sp = c.pastel.blu, underline = true },
+  },
+  { 0, 'DiffDelete', { fg = c.fg[3], bg = c._ } },
+  { 0, 'GSAdd', { fg = c.pastel.gre, bg = c._ } },
+  { 0, 'GSCha', { fg = c.pastel.blu, bg = c._ } },
+  { 0, 'GSDel', { fg = c.pastel.red, bg = c._ } },
+  { 0, 'GSAddNr', { link = 'GitsignsAddNr' } },
+  { 0, 'GSAddLn', { link = 'GitSignsAddLn' } },
+  { 0, 'GSChaNr', { link = 'GitSignsChangeNr' } },
+  { 0, 'GSChaLn', { link = 'GitSignsChangeLn' } },
+  { 0, 'GSDelNr', { link = 'GitSignsDeleteNr' } },
+  { 0, 'GSDelLn', { link = 'GitSignsDeleteLn' } },
 
-    -- Git(signs)
-    {
-      0,
-      'DiffText',
-      { bg = '#3d5665', sp = blu1, underline = true },
-    },
-    { 0, 'DiffDelete', { fg = fg3, bg = _ } },
-    { 0, 'GSAdd', { fg = gre1, bg = _ } },
-    { 0, 'GSCha', { fg = blu1, bg = _ } },
-    { 0, 'GSDel', { fg = red1, bg = _ } },
-    { 0, 'GSAddNr', { link = 'GitsignsAddNr' } },
-    { 0, 'GSAddLn', { link = 'GitSignsAddLn' } },
-    { 0, 'GSChaNr', { link = 'GitSignsChangeNr' } },
-    { 0, 'GSChaLn', { link = 'GitSignsChangeLn' } },
-    { 0, 'GSDelNr', { link = 'GitSignsDeleteNr' } },
-    { 0, 'GSDelLn', { link = 'GitSignsDeleteLn' } },
+  -- telescope
+  { 0, 'TelescopeBorder', { fg = c.fg[3], bg = c._ } },
+  { 0, 'TelescopeMatching', { fg = c.pastel.red, bg = c._ } },
+  { 0, 'TelescopeSelection', { fg = c.fg[1], bg = c.bg[2] } },
+  { 0, 'TelescopeTitle', { fg = c.pastel.red, bg = c._ } },
+  { 0, 'TelescopeNormal', { fg = c.fg[1], bg = c._ } },
+  { 0, 'TelescopePreviewTitle', { fg = c.pastel.blu, bg = c._ } },
+  { 0, 'TelescopePreviewNormal', { fg = c.fg[1], bg = c._ } },
+  { 0, 'TelescopePromptBorder', { fg = c.fg[3], bg = c._ } },
+  { 0, 'TelescopePromptCounter', { fg = c.pastel.gre, bg = c._ } },
+  { 0, 'TelescopePromptNormal', { fg = c.fg[1], bg = c._ } },
+  { 0, 'TelescopePromptPrefix', { fg = c.pastel.gre, bg = c._ } },
+  { 0, 'TelescopePromptTitle', { fg = c.pastel.gre, bg = c._ } },
 
-    -- Telescope
-    { 0, 'TelescopeBorder', { fg = fg3, bg = _ } },
-    { 0, 'TelescopeMatching', { fg = red1, bg = _ } },
-    { 0, 'TelescopeSelection', { fg = fg1, bg = fg5 } },
-    { 0, 'TelescopeTitle', { fg = red1, bg = _ } },
-    { 0, 'TelescopeNormal', { fg = fg1, bg = _ } },
-    { 0, 'TelescopePreviewTitle', { fg = blu1, bg = _ } },
-    { 0, 'TelescopePreviewNormal', { fg = fg1, bg = _ } },
-    { 0, 'TelescopePromptBorder', { fg = fg3, bg = _ } },
-    { 0, 'TelescopePromptCounter', { fg = gre1, bg = _ } },
-    { 0, 'TelescopePromptNormal', { fg = fg1, bg = _ } },
-    { 0, 'TelescopePromptPrefix', { fg = gre1, bg = _ } },
-    { 0, 'TelescopePromptTitle', { fg = gre1, bg = _ } },
+  -- treesitter
+  { 0, 'TreesitterContext', { fg = c._, bg = c.bg[2] } },
+  { 0, 'TreesitterContextLineNumber', { fg = c.fg[3], bg = c._ } },
+}
 
-    -- Treesitter
-    { 0, 'TreesitterContext', { fg = _, bg = fg5 } },
-    { 0, 'TreesitterContextLineNumber', { fg = fg3, bg = _ } },
-  }
-
-  for _, group in pairs(highlights) do
-    vim.api.nvim_set_hl(group[1], group[2], group[3])
-  end
+for i = 1, #highlights do
+  vim.api.nvim_set_hl(highlights[i][1], highlights[i][2], highlights[i][3])
 end
-
-set_hl_groups()
