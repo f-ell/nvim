72c1669dc8f34affd20901a57a11c305401163ab
Nico Pareigis <nico@pareigis.de>
2023-12-31 15:51:03 +0100

    feat: add undotree

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,12 @@
+return {
+  'mbbill/undotree',
+  lazy = true,
+  cmd = 'UndotreeToggle',
+  config = function()
+    local L = require('utils.lib')
+    L.vim.g('undotree_SetFocusWhenToggle', 1)
+    L.vim.g('undotree_ShortIndicators', 1)
+    L.vim.g('undotree_DiffpanelHeight', 16)
+    L.vim.g('undotree_HelpLine', 0)
+  end,
+}
