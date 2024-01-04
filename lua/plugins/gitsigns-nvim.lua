c8d9fa66497ce168201dfbfa32162722066fc91c
Nico Pareigis <nico@pareigis.de>
2024-01-04 16:05:55 +0100

    chore(gitsigns): center hunks after jump

--- lua/plugins/gitsigns-nvim.lua
+++ lua/plugins/gitsigns-nvim.lua
@@ -17,8 +17,8 @@ return {
   event = 'BufReadPost',
   keys = {
     { 'gsh', toggle_diff },
-    { 'gsj', '<CMD>silent Gitsigns next_hunk<CR>' },
-    { 'gsk', '<CMD>silent Gitsigns prev_hunk<CR>' },
+    { 'gsj', '<CMD>silent Gitsigns next_hunk<CR>zz' },
+    { 'gsk', '<CMD>silent Gitsigns prev_hunk<CR>zz' },
     { 'gsl', '<CMD>Gitsigns toggle_deleted<CR>' },
     { 'gsc', '<CMD>Gitsigns toggle_linehl<CR>' },
     { '<leader><', '<CMD>diffget gitsigns://*:0\\\\|2:<CR>' },
