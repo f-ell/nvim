95d01e6cd9a065fae0fbd6017c4a6e0282c1f606
Nico Pareigis <nico@pareigis.de>
2024-01-14 10:18:52 +0100

    chore: enable builtin de-archive/-compression plugins

--- plugin/builtin.lua
+++ plugin/builtin.lua
@@ -1,15 +1,11 @@
 local builtins = {
-  'gzip',
   'man',
-  'matchit',
   'matchparen',
   'netrwPlugin',
   'remote_plugins',
   'shada_plugin',
-  'tarPlugin',
   '2html_plugin',
   'tutor_mode_plugin',
-  'zipPlugin'
 }
 
 for _, plugin in pairs(builtins) do
