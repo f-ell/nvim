84d0401938a8e819bd54cad7a862a5114d27ad4e
Nico Pareigis <nico@pareigis.de>
2024-03-15 22:52:41 +0100

    feat(jdtls): support field and method picking

--- lua/lsp/servers/_jdtls.lua
+++ lua/lsp/servers/_jdtls.lua
@@ -1,6 +1,11 @@
 local M = {}
 
--- TODO: support field picking
+M._util = {
+  signs = vim.fn.filter(vim.fn.sign_getdefined(), function(_, s)
+    return vim.startswith(s.name, 'DiagnosticSign')
+  end),
+}
+
 local function generateToStringPrompt(_, ctx)
   local client = vim.lsp.get_client_by_id(ctx.client_id)
 
@@ -21,13 +26,38 @@ local function generateToStringPrompt(_, ctx)
     return
   end
 
-  _, res = L.lsp.request(
-    client,
-    'java/generateToString',
-    { context = ctx.params, fields = res.result.fields },
-    ctx.bufnr
-  )
-  L.lsp.apply_edit(res[1])
+  local function format(field, selected)
+    return ('%s%s %s'):format(selected and '* ' or '', field.type, field.name)
+  end
+
+  local function callback(fields)
+    err, res = L.lsp.request(
+      client,
+      'java/generateToString',
+      { context = ctx.params, fields = fields },
+      ctx.bufnr
+    )
+
+    if err then
+      L.lsp.notify_error(err)
+      return
+    end
+
+    L.lsp.apply_edit(res[1])
+  end
+
+  local preselect = {}
+  for i = 1, #res.result.fields do
+    table.insert(preselect, i)
+  end
+
+  L.ui.pick(res.result.fields, preselect, format, callback, {
+    title = {
+      { ' ' .. M._util.signs[3].text, M._util.signs[3].texthl },
+      { 'toString ', 'FloatTitle' },
+      { '<ESC> to confirm ', 'NeutralFloat' },
+    },
+  })
 end
 
 -- TODO: support method picking (default: select all not from java.lang.Object)
@@ -49,27 +79,45 @@ local function overrideMethodsPrompt(_, ctx)
     return
   end
 
-  -- local fmt = function(method)
-  --   return ('%s(%s) via %s'):format(
-  --     method.name,
-  --     table.concat(method.parameters, ', '),
-  --     method.declaringClass
-  --   )
-  -- end
-
-  err, res = L.lsp.request(
-    client,
-    'java/addOverridableMethods',
-    { context = ctx.params, overridableMethods = res.result.methods },
-    ctx.bufnr
-  )
-
-  if err then
-    L.lsp.notify_error(err)
-    return
+  local function format(method, selected)
+    return ('%s%s(%s) via %s'):format(
+      selected and '* ' or '',
+      method.name,
+      table.concat(method.parameters, ', '),
+      method.declaringClass
+    )
   end
 
-  L.lsp.apply_edit(res[1])
+  local function callback(methods)
+    err, res = L.lsp.request(
+      client,
+      'java/addOverridableMethods',
+      { context = ctx.params, overridableMethods = methods },
+      ctx.bufnr
+    )
+
+    if err then
+      L.lsp.notify_error(err)
+      return
+    end
+
+    L.lsp.apply_edit(res[1])
+  end
+
+  local preselect = {}
+  for i = 1, #res.result.methods do
+    if res.result.methods[i].declaringClass == 'java.lang.Object' then
+      table.insert(preselect, i)
+    end
+  end
+
+  L.ui.pick(res.result.methods, preselect, format, callback, {
+    title = {
+      { ' ' .. M._util.signs[3].text, M._util.signs[3].texthl },
+      { '@Override ', 'FloatTitle' },
+      { '<ESC> to confirm ', 'NeutralFloat' },
+    },
+  })
 end
 
 M.commands = {
