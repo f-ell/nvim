b4aedcce7281029ea7eccfc62db7412ad89cd999
Nico Pareigis <nico@pareigis.de>
2024-08-09 19:19:52 +0200

    feat(keymap): dec2hex mapping

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,5 +1,33 @@
 local key = L.key
 
+local function dec2hex()
+  vim.api.nvim_feedkeys('viw', 'x', false)
+
+  local s_pos = vim.fn.getpos('v')
+  local e_pos = vim.fn.getpos('.')
+  if s_pos[2] < e_pos[2] or (s_pos[2] == e_pos[2] and s_pos[3] > e_pos[3]) then
+    s_pos, e_pos = e_pos, s_pos
+  end
+
+  vim.api.nvim_buf_set_text(
+    s_pos[1],
+    s_pos[2] - 1,
+    s_pos[3] - 1,
+    e_pos[2] - 1,
+    e_pos[3],
+    { ('0x%x'):format(vim.fn.expand('<cword>')) }
+  )
+
+  vim.api.nvim_feedkeys(
+    vim.api.nvim_replace_termcodes('<ESC>', true, false, true),
+    'x',
+    false
+  )
+  vim.fn.cursor(s_pos[2], s_pos[3])
+end
+
+key.nnmap('<leader>dh', dec2hex)
+
 key.nnmap('--', '<CMD>w<CR>')
 key.nnmap('<leader>~', 'viw~')
 key.nnmap('<leader>w', '<CMD>w !doas tee %<CR>')
