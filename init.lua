66dc26a68251d32ca691050c3218dd39cc9a7902
Nico Pareigis <nico@pareigis.de>
2025-02-18 18:25:39 +0100

    feat: experimental monochrome-ish colours

--- init.lua
+++ init.lua
@@ -34,7 +34,6 @@ require('lazy').setup('plugins', {
 
 L.key.nnmap('<leader>*', '<CMD>Lazy<CR>')
 vim.o.termguicolors = true
-vim.api.nvim_command('colorscheme everforest')
 vim.api.nvim_command('filetype plugin indent on')
 
 require('utils')
--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -5,9 +5,11 @@ return {
     vim.o.background = 'dark'
     vim.g.everforest_background = 'soft'
     vim.g.everforest_enable_bold = '1'
-    vim.g.everforest_enable_italic = '1'
     vim.g.everforest_transparent_background = '1'
     vim.g.everforest_spell_foreground = 'colored'
     vim.g.everforest_better_performance = '1'
+  end,
+  init = function()
+    vim.cmd.colorscheme('everforest')
   end,
 }
