db48b3c4a17f8b4e2e807c18db2eb8160b422d29
Nico Pareigis <nico@pareigis.de>
2025-04-16 18:04:51 +0200

    feat(conform): perl perltidy formatting

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -10,16 +10,16 @@ return {
       javascript = { 'prettierd' },
       lua = { 'stylua' },
       typescript = { 'prettierd' },
+      perl = { 'perltidy' },
     },
     formatters = {
       prettierd = { require_cwd = true },
+      perltidy = { args = { '-pro=.../.perltidyrc' } },
     },
     format_on_save = {
       lsp_format = 'fallback',
       timeout_ms = 500,
       stop_after_first = true,
     },
-    notify_on_error = true,
-    notify_no_formatters = false,
   },
 }
