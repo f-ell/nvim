6cb60436500aef4173a1a165d748c50e598e7e09
Nico Pareigis <nico@pareigis.de>
2026-01-13 18:57:56 +0100

    feat(go): automatic cgo regeneration

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -1,27 +1,15 @@
 vim.bo.expandtab = false
 
-local function organizeImports()
-  local client = vim.lsp.get_clients({ name = 'gopls' })[1]
-  if not client then
-    vim.notify(
-      'Failed to organize imports. No server available.',
-      vim.log.levels.WARN
-    )
-    return
-  end
-
+---@param client vim.lsp.Client
+local function organize_imports(client)
   local params = vim.lsp.util.make_range_params(0, client.offset_encoding) --[[@as table]]
   params.context =
     { diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 }) }
 
   local method = vim.lsp.protocol.Methods.textDocument_codeAction
-  local err, res = L.lsp:request(
-    vim.lsp.get_clients({ bufnr = 0, method = method }),
-    method,
-    params,
-    0
-  )
+  local err, res = L.lsp:request(client, method, params, 0)
   if err then
+    vim.notify('Failed to organize imports.', vim.log.levels.ERROR)
     return
   end
 
@@ -42,6 +30,15 @@ local function organizeImports()
     )
 end
 
+---@param client vim.lsp.Client
+local function regenerate_cgo(client)
+  client:exec_cmd({
+    title = 'Regenerate cgo',
+    command = 'gopls.regenerate_cgo',
+    arguments = { vim.lsp.util.make_text_document_params() },
+  })
+end
+
 vim.api.nvim_create_autocmd('BufWritePre', {
   buffer = vim.api.nvim_get_current_buf(),
   callback = function()
@@ -49,6 +46,16 @@ vim.api.nvim_create_autocmd('BufWritePre', {
       return
     end
 
-    organizeImports()
+    local client = vim.lsp.get_clients({ name = 'gopls' })[1]
+    if not client then
+      vim.notify(
+        'Failed to run hooks. No server available.',
+        vim.log.levels.WARN
+      )
+      return
+    end
+
+    organize_imports(client)
+    regenerate_cgo(client)
   end,
 })
