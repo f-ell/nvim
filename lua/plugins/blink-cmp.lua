9eb10e22f14034b7206c9ffee87e6503554d4ad8
Nico Pareigis <nico@pareigis.de>
2026-02-25 15:50:07 +0100

    fix(blink): update icons for nerdfonts v3 deprecations

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -124,11 +124,11 @@ return {
     appearance = {
       -- stylua: ignore
       kind_icons = {
-        Text     = '', Method = '󰡱', Function  = '', Constructor = '', Field         = '∊',
-        Variable = '󰀫', Class  = '󰠲', Interface = '󱡠', Module      = '', Property      = '󰖷',
-        Unit     = '󰔌', Value  = '', Enum      = '󱀍', Keyword     = '󰻾', Snippet       = '',
-        Color    = '', File   = '󰦨', Reference = '', Folder      = '', EnumMember    = '',
-        Constant = '', Struct = '', Event     = '󱐋', Operator    = '±', TypeParameter = '󰒓',
+        Text     = '', Method = '󰡱', Function  = '󰊕', Constructor = '󰙴', Field         = '∊',
+        Variable = '󰀫', Class  = '󰠲', Interface = '󱡠', Module      = '󰆧', Property      = '󰖷',
+        Unit     = '󰿦', Value  = '󰎠', Enum      = '󱀍', Keyword     = '󰻾', Snippet       = '',
+        Color    = '', File   = '󰦨', Reference = '', Folder      = '󰉋', EnumMember    = '󰀬',
+        Constant = '', Struct = '󰅩', Event     = '', Operator    = '±', TypeParameter = '󰒓',
       },
     },
   },
