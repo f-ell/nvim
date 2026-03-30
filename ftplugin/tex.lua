ca09fa0a6a160a49c3be3bdca4d2b05f1e5d8757
Nico Pareigis <nico@pareigis.de>
2026-03-30 15:40:29 +0200

    fix(ftplugin): localize spell to active buffer only

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -2,7 +2,7 @@ if vim.fn.expand('%:e') == 'sty' then
   return
 end
 
-vim.o.spell = true
+vim.wo[0][0].spell = true
 vim.opt.formatoptions:append('or')
 
 vim.api.nvim_buf_create_user_command(
--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,2 +1,2 @@
-vim.o.spell = true
+vim.wo[0][0].spell = true
 vim.opt.formatoptions:append('or')
