ce776715f31e9cce6001ddc351d9f947b72cb309
Nico Pareigis <nico@pareigis.de>
2026-03-30 15:39:26 +0200

    feat(markdown): increased textwidth in tables

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,1 +1,33 @@
-vim.o.spell = true
+vim.wo[0][0].spell = true
+
+local tw = vim.bo.textwidth
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  buffer = vim.api.nvim_get_current_buf(),
+  callback = function()
+    tw = vim.bo.textwidth
+  end,
+})
+
+local q = vim.treesitter.query.parse('markdown', '(pipe_table) @table')
+
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+  buffer = vim.api.nvim_get_current_buf(),
+  callback = function()
+    local t = vim.treesitter.get_parser():parse()[1]
+    local pos = vim.fn.getpos('.')
+
+    local b = vim.iter(q:iter_captures(t:root(), 0)):any(
+      ---@param id integer
+      ---@param node TSNode
+      ---@param meta vim.treesitter.query.TSMetadata
+      ---
+      ---@diagnostic disable-next-line: unused-local
+      function(id, node, meta)
+        return vim.treesitter.is_in_node_range(node, pos[2] - 1, pos[3] - 1)
+      end
+    )
+
+    vim.bo.textwidth = b and 9999 or tw
+  end,
+})
