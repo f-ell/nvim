97a9c2d98ee2f289b10a7fdeb0ab6e57e62b345a
Nico Pareigis <nico@pareigis.de>
2026-06-16 13:44:05 +0200

    feat(typst): omit images from compilation by default

--- lsp/tinymist.lua
+++ lsp/tinymist.lua
@@ -1,4 +1,8 @@
+---@type vim.lsp.Config
 return {
+  -- Configuration is only valid inside `settings`, despite what the lspconfig
+  -- type information claims.
+  ---@type _.lspconfig.settings.tinymist.Tinymist
   settings = {
     exportPdf = 'onSave',
     formatterPrintWidth = 80,
@@ -8,5 +12,6 @@ return {
       when = 'onType',
     },
     projectResolution = 'lockDatabase',
+    typstExtraArgs = { '--input', 'img=0' },
   },
 }
