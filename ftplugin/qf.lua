7cd1b3abf0de4735408b1485d9d55c10a0f5b1d1
Nico Pareigis <nico@pareigis.de>
2026-07-16 14:48:58 +0200

    fix(qf): bounds checking

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,6 +1,15 @@
 L.key.nnmap('<C-j>', function()
+  if vim.fn.getpos('.')[2] == vim.fn.line('$') then
+    return
+  end
+
   vim.cmd('silent ' .. vim.v.count .. 'cnext | wincmd p')
 end, { buffer = true })
+
 L.key.nnmap('<C-k>', function()
+  if vim.fn.getpos('.')[2] == 1 then
+    return
+  end
+
   vim.cmd('silent ' .. vim.v.count .. 'cprev | wincmd p')
 end, { buffer = true })
